4ab8cde05f13a3492bf7f8536f25d1ce215a8129
João Victor <joaovictordsantosr@gmail.com>
2024-10-21 20:40:47 -0300

    Adicionando bibliotecas e funções iniciais

--- main.c
+++ main.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 #include <time.h>
+#define QTD_ITENS 100
+
+int vetor[QTD_ITENS];
+
+void registrarTempo(char nome_funcao, double tempo_cpu)
+{
+    FILE *arq = fopen("tempos_de_execucao.txt", "a");
+    if (arq == NULL)
+    {
+        perror("Erro ao abrir o arquivo");
+        return;
+    }
+
+    fprintf(arq, "ALGORITMO: %s\n", nome_funcao);
+    fprintf(arq, "TEMPO DE EXECUÇÃO: %f\n", tempo_cpu);
+    fclose(arq);
+}
 
 // 1 - Selection Sort
 void selectionSort(int *v)
@@ -15,11 +32,26 @@ void selectionSort(int *v)
                 min = j;
             }
         }
-        if (i != min)
-        {
-            aux = v[i];
-            v[i] = v[min];
-            v[min] = aux;
-        }
+        aux = v[min];
+        v[min] = v[i];
+        v[i] = aux;
     }
+}
+
+int main()
+{
+    for (int i = 0; i < QTD_ITENS; i++)
+    {
+        vetor[i] = rand() % QTD_ITENS;
+    }
+    clock_t inicio, fim;
+    double tempo_cpu;
+
+    inicio = clock();
+    // algoritmo de ordenação
+    fim = clock();
+    tempo_cpu = ((double)(fim - inicio)) / CLOCKS_PER_SEC;
+    registrarTempo("Selection Sort", tempo_cpu);
+
+    return 0;
 }
