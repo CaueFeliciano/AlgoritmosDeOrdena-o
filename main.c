2ebe05d9bcba50d6d32f5e604f8d4e3084979771
Caue Feliciano <cauekenzo@hotmail.com>
2024-10-31 21:32:56 -0300

    Adcionando as funções 4 (merge sort) e 5 (quick sort).

--- main.c
+++ main.c
@@ -146,10 +146,89 @@ void bubbleSort(int *v)
 }
 
 // 4 - Merge Sort
+//OBJETIVO: Criar uma sequência ordenada a partir de duas outras, também ordenadas. Ele divide a sequência em pares de dados, ordenando-as; para depois agrupa-lás. Até chegar em duas partes e então combinar elas.
+void merge(int vetor[], int inicio, int meio, int fim){ //Função merge, é responsável por unir duas metades de um vetor em ordem.
+    int tamanho = (fim - inicio) + 1; //Cálculo do tmanho do subarray
+    int i, j, posicao;
+    int temp[tamanho]; //Array temporario para armazenar os elementos do subarray
+    
+    for (i = inicio; i <= fim; i++){
+        temp[i - inicio] = vetor [i];
+    }//Vai copiar os elementos do vetor origanl, vetor, para o array temporario
+    
+    i = 0; //Reinicia o índice para ser usado em uma das metades do vetor no array temp
+    j = meio - inicio + 1; //Faz a mesma coisa que o i para a segunda metade do vetor no vator array temp
+    posicao = inicio; //Inicia para ser usado na inserção dos elementos de volta no vetor original
+    
+    for (;i <= meio - inicio && j <= fim - inicio; posicao++){ //Este for contiua enquanto tiver elementos em ambas as metades
+        if(temp[i] < temp[j]){ 
+            vetor[posicao] = temp[i];
+            i++;
+        }
+        else{
+            vetor[posicao] = temp[j];
+            j++;
+        }
+    }//Se a primeira metade de temp[i] for menor o da, segunda tem[j], ele é adcionado; se não o elemento da segunda metade é inserido
+    
+    for (; i <= meio - inicio; i++, posicao++) {
+        vetor[posicao] = temp[i];
+    }//Caso tenha elementos na primeira metade, ele copia os restantes para o vetor original
+    
+    for (; j <= fim - inicio; j++, posicao++) {
+        vetor[posicao] = temp[j];
+    }//Faz a mesma coisa que o ultimo for, porém na segunda metade
+}
 
+void mergeSort(int *v){//Função que realiza a ordenação recursiva
+    int meio;
+    int esq = v[0];
+    int dir = v[QTD_ITENS - 1];
+    if(dir > esq){//garante que tem mais de um elemtento para ordenar. Se não, a função não faz nada
+        meio = (dir + esq )/2;
+        mergeSort(v, esq, meio);
+        mergeSort(v, meio + 1, dir);//Divide o vetor ao meio, para as duas metades até que restam apenas um elemento
+        merge(v, esq, meio, dir);//Uni as duas metades em ordem 
+    }
+}
 
 // 5 - Quick Sort
+//OBJETIVO: Dividir o problema de ordenar um conjunto com n itens em dois problemas menores, para esses menores serem ordenados independentemente.
+typedef struct {
+    int Chave;
+} Item;//Estrutura que permite que o código lide com mais campos e organiza os elementos que serão ordenados.
 
+void Particao(int Esq, int Dir, int *i, int *j, Item *A)//Essa função realiza a partição do vetor, por meio de um valor pivo.
+{
+    Item x, aux;//x sera nosso pivo e aux uma variável auxiliar usada para troca.
+    *i = Esq;//i representa é um ponteiro que indica a posição do índice de participaração, que ai estamos falando o Esq vai receber este valor.
+    *j = Dir;//j também reprenta um ponteiro que indica a posição do índice de participação.
+    x = A[(*i + *j) / 2]; // obtem o pivo x 
+    do { 
+        while (x.Chave > A[*i].Chave) (*i)++;//Este primeiro while move o *i da esquerda para direita enquanto os valores em A[*i] forem menores que o pivo x. Quando esse valores atingem um número maior ou igual ao pivo  o while termina e o i passa para a posição.
+        while (x.Chave < A[*j].Chave) (*j)--;//Este while faz a mesma coisa que o anterior porém com o *j e ao invés da esquerda para a direita ele passa da direita para a esquerda.
+        if (*i <= *j) {//Se *i for maior ou igual a *j, o A[*i] troca com A[*j].
+            aux = A[*i];//aux armazena temporariamente o valor que esta em A[*i].
+            A[*i] = A[*j];//O valor de A[*j] é copiado para A[*i], movendo o valor maior para a posição correta.
+            A[*j] = aux;//O valor que foi armazenado no aux é realocado em A[j*].
+            (*i)++;//Após a troca é acrescentado +1 a *i movendo-o para a direita; continuando o processo.
+            (*j)--;//Após a troca é decrescentado 1 a *j movendo-o para a esquerda; continuando o processo.
+        }
+    } while (*i <= *j);//Encerra o loop quando *i for maior ou igual ao *j.
+}
+
+void Ordena(int Esq, int Dir, Item *A)//Essa função ordena cada subparte que a função Particao dividi.
+{
+    int i, j;//As variáveis i e j declaradas vão receber os índices atualizados após a partição.
+    Particao(Esq, Dir, &i, &j, A);//Chama a função Particao para dividir o vetor A entre Esq e Dir 
+    if (Esq < j) Ordena(Esq, j, A);//Verifica se ainda existe algum elemento à esquerda de j que precisam ser ordenados.
+    if (i < Dir) Ordena(i, Dir, A);//Verifica se ainda a algum elemendo à direita de i que precisam ser ordenados.
+}
+
+void QuickSort(Item *A, int n)
+{
+    Ordena(0, n - 1, A);//Essa função inicia o processo de ordenação do vetor A chamando a Ordena com os valores iniciais 0 e n-1
+}
 
 // 6 - Heap Sort
 // OBJETIVO: Transformar o vetor em uma árvore binária e ordená-la.
@@ -180,6 +259,8 @@ int main()
     medirTempo(selectionSort, "Selection Sort");
     medirTempo(insertionSort, "Insertion Sort");
     medirTempo(bubbleSort, "Bubble Sort");
+    medirTempo(mergeSort, "Merge Sort");
+    medirTempo(QuickSort, "Quick Sort");
     medirTempo(heapSort, "Heap Sort");
 
     return 0;
